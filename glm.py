e232cad52f1e4bb5ded3204032a36232b0d7930e
Corbett Bennett <corbettb@alleninstitute.org>
2019-09-10 16:12:09 -0700

    Update glm.py

--- glm.py
+++ glm.py
@@ -6,15 +6,15 @@ Created on Thu Aug 22 14:18:26 2019
 """
 from __future__ import division
 import getData
-import summaryPlots
 from matplotlib import pyplot as plt
 import probeSync
 import licking_model
-from numba import njit
 import autograd.numpy as np
 from autograd import grad
 from scipy.optimize import minimize
 import copy
+import numpy as npo
+from numba import njit
 
 #using our class for parsing the data (https://github.com/corbennett/ephys_behavior)
 b = getData.behaviorEphys('Z:\\05162019_423749')
@@ -23,19 +23,22 @@ b.loadFromHDF5(r"Z:\analysis\05162019_423749.hdf5")
 def binVariable(var, binwidth=0.001):
     return np.histogram(var, bins=np.arange(0, b.lastBehaviorTime, binwidth))[0]
 
+def binRunning(runSig, runTime, binwidth=0.001):
+    return np.interp(np.arange(0, b.lastBehaviorTime, binwidth), runTime, runSig)[:-1]
+    
 @njit
-def meanPSTH(binned_spikes, binned_times, binwidth, preTime, postTime):
+def getPSTH(binned_spikes, binned_times, binwidth, preTime, postTime):
     preTime_bin = int(preTime/binwidth)
     postTime_bin = int(postTime/binwidth)
-    eventTimes = np.flatnonzero(binned_times)
-    psth = np.zeros(preTime_bin+postTime_bin)
+    eventTimes = npo.where(binned_times)[0]
+    psth = npo.zeros(preTime_bin+postTime_bin)
     for i, t in enumerate(eventTimes):
         trace = binned_spikes[t-preTime_bin:t+postTime_bin]
         psth = psth + trace
     
     return psth/len(eventTimes)
-        
-def find_inital_guess(binned_spikes, binned_times, bin_width, ffilter):
+
+def find_initial_guess(psth, ffilter):
     def compute_error(latent, inputs):
         return np.sum((latent-inputs)**2)
     
@@ -44,8 +47,6 @@ def find_inital_guess(binned_spikes, binned_times, bin_width, ffilter):
         latent = ffilter.build_filter()
         return compute_error(latent, np.log(psth))
     
-    psth = meanPSTH(binned_spikes, binned_times, bin_width, 0, ffilter.duration)[1:]/np.mean(binned_spikes) + np.finfo(float).eps
-    
     params=ffilter.params
     g = grad(to_min)
     res = minimize(to_min, params, jac=g)
@@ -53,7 +54,7 @@ def find_inital_guess(binned_spikes, binned_times, bin_width, ffilter):
     return res['x']
 
 
-binwidth = 0.1
+binwidth = 0.02
 spikes = b.units['C']['270']['times']
 binned_spikes = binVariable(spikes[spikes<b.lastBehaviorTime], binwidth)
 
@@ -62,51 +63,69 @@ model = licking_model.Model(dt=binwidth, licks=binned_spikes)
 selectedTrials = (b.hit | b.miss)&(~b.ignore)   #Omit "ignore" trials (aborted trials when the mouse licked too early or catch trials when the image didn't actually change)
 active_changeTimes = b.frameAppearTimes[np.array(b.trials['change_frame'][selectedTrials]).astype(int)+1] #add one here to correct for a one frame shift in frame times from camstim
 binned_activeChangeTimes = binVariable(active_changeTimes, binwidth)
-changeFilter = licking_model.GaussianBasisFilter(num_params=10, data=binned_activeChangeTimes, dt=model.dt, duration=0.5, sigma=0.05)
+changeFilter = licking_model.GaussianBasisFilter(num_params=10, data=binned_activeChangeTimes, dt=model.dt, duration=0.6, sigma=0.05)
 model.add_filter('change', changeFilter)
 
-lick_times = probeSync.get_sync_line_data(b.syncDataset, 'lick_sensor')[0]
+lick_offset = 0.25
+lick_times = probeSync.get_sync_line_data(b.syncDataset, 'lick_sensor')[0] - lick_offset
 binned_licks = binVariable(lick_times, binwidth)
-lickFilter = licking_model.GaussianBasisFilter(num_params=5, data=binned_licks, dt=model.dt, duration=0.25, sigma=0.05)
+lickFilter = licking_model.GaussianBasisFilter(num_params=5, data=binned_licks, dt=model.dt, duration=0.6, sigma=0.1)
 model.add_filter('lick', lickFilter)
 
+binned_running = binRunning(b.behaviorRunSpeed.values, b.behaviorRunTime, binwidth)
+runFilter = licking_model.GaussianBasisFilter(num_params=5, data=binned_licks, dt=model.dt, duration=1, sigma=0.2)
+model.add_filter('run', runFilter)
 
 flash_times = b.frameAppearTimes[np.array(b.core_data['visual_stimuli']['frame'])]
 image_id = np.array(b.core_data['visual_stimuli']['image_name'])
 binned_flashTimes = []
-for i,img in enumerate(b.imageNames):
+for i,img in enumerate(np.unique(image_id)):
     this_image_times = flash_times[image_id==img]
-    binned_flashTimes.append(binVariable(this_image_times, binwidth))
-
-for bf, imname in zip(binned_flashTimes, b.imageNames):
-    flash_filter = licking_model.GaussianBasisFilter(num_params=10, data=bf, dt=model.dt, duration=0.5, sigma=0.05)
-    model.add_filter('flash_' + imname, flash_filter)
+    bf = binVariable(this_image_times, binwidth)
+    binned_flashTimes.append(bf)
+    flash_filter = licking_model.GaussianBasisFilter(num_params=10, data=bf, dt=model.dt, duration=0.6, sigma=0.05)
+    model.add_filter('flash_' + img, flash_filter)
 
 #set initial guesses
 for filter_name, ffilter in model.filters.items():
+    if filter_name=='run':
+        continue
     binned_times = ffilter.data
-    init_guess = find_inital_guess(binned_spikes, binned_times, binwidth, ffilter)
-#    init_guess[np.abs(init_guess)>0.5] = 0
+    thispsth = getPSTH(binned_spikes, binned_times, binwidth, 0, ffilter.duration)[1:]/np.mean(binned_spikes) + np.finfo(float).eps
+    init_guess = find_initial_guess(thispsth, ffilter)
     ffilter.set_params(init_guess)
     
 #fit model    
-model.mean_rate_param = np.log(np.mean(binned_spikes))
+model.mean_rate_param = np.log(np.mean(binned_spikes)/binwidth)
 model.verbose=True
+model.l2=10
 model.fit()
 plt.figure()
 model.plot_filters()
-
 
 
 #see how close initial guesses were
 init_params = []
 for filter_name, ffilter in model.filters.items():
     binned_times = ffilter.data
+    thispsth = getPSTH(binned_spikes, binned_times, binwidth, 0, ffilter.duration)[1:]/np.mean(binned_spikes) + np.finfo(float).eps
+
     init_guess_filt = copy.deepcopy(ffilter)
-    init_guess = find_inital_guess(binned_spikes, binned_times, binwidth, init_guess_filt)
+#    init_guess = find_initial_guess(binned_spikes, binned_times, binwidth, init_guess_filt)
+    init_guess = find_initial_guess(thispsth, init_guess_filt)
     init_guess_filt.set_params(init_guess)
     init_params.extend(init_guess)
     fig, ax = plt.subplots()
     ax.plot(np.exp(init_guess_filt.build_filter()))
     ax.plot(np.exp(ffilter.build_filter()))
     ax.set_title(filter_name)
+
+init_params.insert(0,np.log(np.mean(binned_spikes)/binwidth))
+init_params = np.array(init_params)
+fig, ax = plt.subplots()
+ax.plot(init_params, model.get_filter_params(), 'ko')
+ax.set_aspect('equal')
+lims = [np.min([ax.get_xlim(), ax.get_ylim()]),np.max([ax.get_xlim(), ax.get_ylim()])]
+ax.plot(lims, lims, 'k--')
+ax.set_ylabel('final fit params')
+ax.set_xlabel('initial guess params')
