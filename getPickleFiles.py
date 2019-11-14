dba882e87be44b4a2c94665ace4fdc0c831ce2cb
Corbett Bennett <corbettb@alleninstitute.org>
2019-11-14 09:55:07 -0800

    a

--- getPickleFiles.py
+++ getPickleFiles.py
@@ -33,20 +33,20 @@ def getPickleFiles(mouseID,src=r'C:\ProgramData\camstim\output',dst=r'\\EphysRoo
 
 
 
-#Get LIMS specimen ID 
-mouse_ids =  (('429084',('07112019','07122019')),
-             ('423744',('08082019','08092019')),
-             ('423750',('08132019','08142019')),
-             ('459521',('09052019','09062019')),
-             ('461027',('09122019','09132019')),
-             )
-
-for mouse, ephysdates in mouse_ids:
-    json_string = urllib.urlopen("http://lims2/specimens/isi_experiment_details/" + mouse + ".json").read()
-    info = json.loads(json_string)            
-    specimen_id = info[0]['id']
-    
-    limsDir = r'\\allen\programs\braintv\production\visualbehavior\prod0\specimen_' + specimen_id
-    getPickleFiles(mouse,limsDir)
-    
+##Get LIMS specimen ID 
+#mouse_ids =  (('429084',('07112019','07122019')),
+#             ('423744',('08082019','08092019')),
+#             ('423750',('08132019','08142019')),
+#             ('459521',('09052019','09062019')),
+#             ('461027',('09122019','09132019')),
+#             )
+#
+#for mouse, ephysdates in mouse_ids:
+#    json_string = urllib.urlopen("http://lims2/specimens/isi_experiment_details/" + mouse + ".json").read()
+#    info = json.loads(json_string)            
+#    specimen_id = info[0]['id']
+#    
+#    limsDir = r'\\allen\programs\braintv\production\visualbehavior\prod0\specimen_' + specimen_id
+#    getPickleFiles(mouse,limsDir)
+#    
    
