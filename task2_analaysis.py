ba1800ed021e7fdc48586ccede4e4c63e157f981
Sam Gale <samueldgale@gmail.com>
2020-06-09 18:24:26 -0700

    Update task2_analaysis.py

--- task2_analaysis.py
+++ task2_analaysis.py
@@ -18,67 +18,70 @@ pkl = pd.read_pickle(f)
 
 params = pkl['items']['behavior']['params']
 
-#sorted(params.items())
-
-changeLog = pkl['items']['behavior']['stimuli']['grating']['change_log']
-
-licks = pkl['items']['behavior']['lick_sensors'][0]['lick_event']
-
 frameIntervals = pkl['items']['behavior']['intervalsms']
-
-
 
 trialLog = np.array(pkl['items']['behavior']['trial_log'][:-1])
 
 
+trialStartTimes = np.full(len(trialLog),np.nan)
+trialEndTimes = np.full(len(trialLog),np.nan)
+scheduledChangeTimes = np.full(len(trialLog),np.nan)
+changeTimes = np.full(len(trialLog),np.nan)
+abortedTrials = np.zeros(len(trialLog),dtype=bool)
+abortTimes = np.full(len(trialLog),np.nan)
+catchTrials = np.zeros(len(trialLog),dtype=bool)
+rewardTimes = np.full(len(trialLog),np.nan)
+autoReward = np.zeros(len(trialLog),dtype=bool)
+hit = np.zeros(len(trialLog),dtype=bool)
+miss = np.zeros(len(trialLog),dtype=bool)
+falseAlarm = np.zeros(len(trialLog),dtype=bool)
+correctReject = np.zeros(len(trialLog),dtype=bool)
+for i,trial in enumerate(trialLog):
+    for event,epoch,t,frame in trial['events']:
+        if event=='trial_start':
+            trialStartTimes[i] = t
+        elif event=='trial_end':
+            trialEndTimes[i] = t
+        elif event=='stimulus_window' and epoch=='enter':
+            scheduledChangeTimes[i] = t + trial['trial_params']['change_time']
+        elif event in ('stimulus_changed','sham_change'):
+            changeTimes[i] = t
+        elif event=='abort':
+            abortedTrials[i] = True
+            abortTimes[i] = t
+        elif event=='hit':
+            hit[i] = True
+        elif event=='miss':
+            miss[i] = True 
+        elif event=='false_alarm':
+            falseAlarm[i] = True
+        elif event=='rejection':
+            correctReject[i] = True
+    catchTrials[i] = trial['trial_params']['catch']
+    if len(trial['rewards'])>0:
+        rewardTimes[i] = trial['rewards'][0][1]
+        autoReward[i] = trial['trial_params']['auto_reward']
 
-trialStartTimes = np.array([event[2] for trial in trialLog for event in trial['events'] if event[0]=='trial_start'])
 
-trialEndTimes = np.array([event[2] for trial in trialLog for event in trial['events'] if event[0]=='trial_end'])
-
-scheduledChangeTimes = np.array([event[2] + trial['trial_params']['change_time'] for trial in trialLog for event in trial['events'] if event[0]=='stimulus_window' and event[1]=='enter'])
-
-changeTimes = np.array([event[2] for trial in trialLog for event in trial['events'] if event[0] in ('stimulus_changed','sham_change')])
-
-changeTrials = np.array(['stimulus_changed' in [event[0] for event in trial['events']] for trial in trialLog])
-
-catchTrials = np.array(['sham_change' in [event[0] for event in trial['events']] for trial in trialLog])
-
-abortedTrials = np.array(['abort' in [event[0] for event in trial['events']] for trial in trialLog])
-
-abortTimes = np.array([event[2] for trial in trialLog for event in trial['events'] if event[0]=='abort'])
-
-incompleteTrials = np.array([not any(np.in1d(('stimulus_changed','sham_change'),[event[0] for event in trial['events']])) for trial in trialLog])
-
-
-
-
-# loop through trials and add value or nan for each param
-
-
-
-timeToChange = changeTimes-trialStartTimes[~incompleteTrials]
+timeToChange = changeTimes-trialStartTimes
 
 interTrialInterval = trialStartTimes[1:]-trialStartTimes[:-1]
 
-timeFromChangeToTrialEnd = trialEndTimes[~incompleteTrials]-changeTimes
+timeFromChangeToTrialEnd = trialEndTimes-changeTimes
 
-timeFromAbortToTrialEnd = trialEndTimes[abortedTrials]-abortTimes
-
-
+timeFromAbortToTrialEnd = trialEndTimes-abortTimes
 
 
-
-fig = plt.figure(figsize=(5,6))
+fig = plt.figure(figsize=(7,7))
 ax = fig.add_subplot(4,1,1)
-ax.hist(timeToChange[(changeTrials & ~abortedTrials)[~incompleteTrials]],bins=np.arange(0,10,0.17),color='g',label='change (n='+str(np.sum(changeTrials & ~abortedTrials))+')')
-ax.hist(timeToChange[(catchTrials & ~abortedTrials)[~incompleteTrials]],bins=np.arange(0,10,0.17),color='r',label='catch (n='+str(np.sum(catchTrials & ~abortedTrials))+')')
+ax.hist(timeToChange[~catchTrials & ~abortedTrials],bins=np.arange(0,10,0.17),color='g',label='change (n='+str(np.sum(~catchTrials & ~abortedTrials))+')')
+ax.hist(timeToChange[catchTrials & ~abortedTrials],bins=np.arange(0,10,0.17),color='r',label='catch (n='+str(np.sum(catchTrials & ~abortedTrials))+')')
 for side in ('right','top'):
     ax.spines[side].set_visible(False)
 ax.tick_params(direction='out',top=False,right=False)
 ax.set_xlim([0,7])
 ax.set_xlabel('Time to change/catch from trial start (s)')
-ax.set_ylabel('Number of trials')
+ax.set_ylabel('Trials')
 ax.set_title(params['stage'])
 ax.legend()
 
@@ -89,28 +92,29 @@ for side in ('right','top'):
 ax.tick_params(direction='out',top=False,right=False)
 ax.set_xlim([interTrialInterval.min()-1,interTrialInterval.max()+1])
 ax.set_xlabel('Inter-trial interval (start to start) (s)')
-ax.set_ylabel('Number of trials')
+ax.set_ylabel('Trials')
 plt.tight_layout()
 
 ax = fig.add_subplot(4,1,3)
-t = timeFromChangeToTrialEnd[~abortedTrials[~incompleteTrials]]
+t = timeFromChangeToTrialEnd[~abortedTrials]
 ax.hist(t,bins=np.arange(0,10,0.17),color='k')
 for side in ('right','top'):
     ax.spines[side].set_visible(False)
 ax.tick_params(direction='out',top=False,right=False)
 ax.set_xlim([t.min()-1,t.max()+1])
-ax.set_xlabel('Time from change/catch to trial end (includes random gray) (s)')
-ax.set_ylabel('Number of trials')
+ax.set_xlabel('Time from change/catch to trial end (includes response window + random gray) (s)')
+ax.set_ylabel('Trials')
 plt.tight_layout()
 
 ax = fig.add_subplot(4,1,4)
-ax.hist(timeFromAbortToTrialEnd,bins=np.arange(0,10,0.17),color='k')
+t = timeFromAbortToTrialEnd[abortedTrials]
+ax.hist(t,bins=np.arange(0,10,0.17),color='k')
 for side in ('right','top'):
     ax.spines[side].set_visible(False)
 ax.tick_params(direction='out',top=False,right=False)
-ax.set_xlim([timeFromAbortToTrialEnd.min()-1,timeFromAbortToTrialEnd.max()+1])
-ax.set_xlabel('Time from abort to trial end (includes random gray) (s)')
-ax.set_ylabel('Number of trials')
+ax.set_xlim([t.min()-1,t.max()+1])
+ax.set_xlabel('Time from abort to trial end (includes timeout + random gray) (s)')
+ax.set_ylabel('Trials')
 plt.tight_layout()
 
 
