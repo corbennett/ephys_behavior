278167c505ba194b9cb45fe629179134e9212536
Sam Gale <samueldgale@gmail.com>
2020-06-11 13:20:45 -0700

    Update task2_analaysis.py

--- task2_analaysis.py
+++ task2_analaysis.py
@@ -15,11 +15,8 @@ f = fileIO.getFile()
 pkl = pd.read_pickle(f)
 
 
-
 params = pkl['items']['behavior']['params']
-
 frameIntervals = pkl['items']['behavior']['intervalsms']
-
 trialLog = np.array(pkl['items']['behavior']['trial_log'][:-1])
 
 
@@ -58,19 +55,16 @@ for i,trial in enumerate(trialLog):
         elif event=='rejection':
             correctReject[i] = True
     catchTrials[i] = trial['trial_params']['catch']
-    if len(trial['rewards'])>0:
+    if len(trial['rewards']) > 0:
         rewardTimes[i] = trial['rewards'][0][1]
         autoReward[i] = trial['trial_params']['auto_reward']
 
 
-timeToChange = changeTimes-trialStartTimes
-
-interTrialInterval = trialStartTimes[1:]-trialStartTimes[:-1]
-
-timeFromChangeToTrialEnd = trialEndTimes-changeTimes
-
-timeFromAbortToTrialEnd = trialEndTimes-abortTimes
-
+# task timing
+timeToChange = changeTimes - trialStartTimes
+interTrialInterval = trialStartTimes[1:] - trialStartTimes[:-1]
+timeFromChangeToTrialEnd = trialEndTimes - changeTimes
+timeFromAbortToTrialEnd = trialEndTimes - abortTimes
 
 fig = plt.figure(figsize=(7,7))
 ax = fig.add_subplot(4,1,1)
@@ -79,7 +73,7 @@ ax.hist(timeToChange[catchTrials & ~abortedTrials],bins=np.arange(0,10,0.17),col
 for side in ('right','top'):
     ax.spines[side].set_visible(False)
 ax.tick_params(direction='out',top=False,right=False)
-ax.set_xlim([0,7])
+ax.set_xlim([0,round(timeToChange[~abortedTrials].max())+1])
 ax.set_xlabel('Time to change/catch from trial start (s)')
 ax.set_ylabel('Trials')
 ax.set_title(params['stage'])
@@ -90,29 +84,30 @@ ax.hist(interTrialInterval,bins=np.arange(0,10,0.17),color='k')
 for side in ('right','top'):
     ax.spines[side].set_visible(False)
 ax.tick_params(direction='out',top=False,right=False)
-ax.set_xlim([interTrialInterval.min()-1,interTrialInterval.max()+1])
+ax.set_xlim([round(interTrialInterval.min())-1,round(interTrialInterval.max())+1])
 ax.set_xlabel('Inter-trial interval (start to start) (s)')
 ax.set_ylabel('Trials')
 plt.tight_layout()
 
 ax = fig.add_subplot(4,1,3)
-t = timeFromChangeToTrialEnd[~abortedTrials]
-ax.hist(t,bins=np.arange(0,10,0.17),color='k')
+changeToEnd = timeFromChangeToTrialEnd[~abortedTrials]
+abortToEnd = timeFromAbortToTrialEnd[abortedTrials]
+xlim = [round(min(changeToEnd.min(),abortToEnd.min()))-1,round(max(changeToEnd.max(),abortToEnd.max()))+1]
+ax.hist(changeToEnd,bins=np.arange(0,10,0.17),color='k')
 for side in ('right','top'):
     ax.spines[side].set_visible(False)
 ax.tick_params(direction='out',top=False,right=False)
-ax.set_xlim([t.min()-1,t.max()+1])
+ax.set_xlim(xlim)
 ax.set_xlabel('Time from change/catch to trial end (includes response window + random gray) (s)')
 ax.set_ylabel('Trials')
 plt.tight_layout()
 
 ax = fig.add_subplot(4,1,4)
-t = timeFromAbortToTrialEnd[abortedTrials]
-ax.hist(t,bins=np.arange(0,10,0.17),color='k')
+ax.hist(abortToEnd,bins=np.arange(0,10,0.17),color='k')
 for side in ('right','top'):
     ax.spines[side].set_visible(False)
 ax.tick_params(direction='out',top=False,right=False)
-ax.set_xlim([t.min()-1,t.max()+1])
+ax.set_xlim(xlim)
 ax.set_xlabel('Time from abort to trial end (includes timeout + random gray) (s)')
 ax.set_ylabel('Trials')
 plt.tight_layout()
