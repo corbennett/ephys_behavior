c2406b2fd694a9ba5b0aafd1f5d86a337f79440f
Sam Gale <samueldgale@gmail.com>
2020-12-04 17:30:48 -0800

    Update lasertest.py

--- lasertest.py
+++ lasertest.py
@@ -14,9 +14,6 @@ from sync import sync
 import probeSync
 
 
-syncFile = fileIO.getFile('choose sync file',fileType='*.h5')
-syncDataset = sync.Dataset(syncFile)
-
 pklFile = fileIO.getFile('choose pkl file',fileType='*.pkl')
 pkl = pd.read_pickle(pklFile)
 
@@ -26,38 +23,12 @@ if params['periodic_flash'] is not None:
     flashInterval = flashDur + grayDur
 
 frameRate = 60
-
-frameRising, frameFalling = probeSync.get_sync_line_data(syncDataset, 'vsync_stim')
-vsyncTimes = frameFalling[1:] if frameFalling[0] < frameRising[0] else frameFalling
 monitorLag = params['laser_params']['monitor_lag']/frameRate
-frameAppearTimes = vsyncTimes + monitorLag
-
-laserRising,laserFalling = probeSync.get_sync_line_data(syncDataset,channel=11)
 
 trialLog = pkl['items']['behavior']['trial_log']
 laserLog = pkl['items']['behavior']['layzer_trials']
 changeLog = pkl['items']['behavior']['stimuli']['images']['change_log']
-
-#trialStartTimes = []
-#changeTimes = []
-#catchTrials = []
-#laserTrials = []
-#laserFrameTimes = []
-#for trial,laser in zip(trialLog,laserLog): # not same length; don't do this
-#    catchTrials.append(trial['trial_params']['catch'])
-#    for event,epoch,t,frame in trial['events']:
-#        if event=='trial_start':
-#            trialStartTimes.append(vsyncTimes[frame])
-#        elif event in ('stimulus_changed','sham_change'):
-#            changeTimes.append(frameAppearTimes[frame])
-#    if 'actual_layzer_frame' in laser:
-#        laserTrials.append(True)
-#        laserFrameTimes.append(vsyncTimes[laser['actual_layzer_frame']])
-#    else:
-#        laserTrials.append(False)
-#        laserFrameTimes.append(np.nan)
-        
-        
+             
 trialStartTimes = np.full(len(trialLog),np.nan)
 trialStartFrames = np.full(len(trialLog),np.nan)
 trialEndTimes = np.full(len(trialLog),np.nan)
@@ -78,7 +49,7 @@ falseAlarm = np.zeros(len(trialLog),dtype=bool)
 correctReject = np.zeros(len(trialLog),dtype=bool)
 laserTrials = np.zeros(len(trialLog),dtype=bool)
 laserOffset = np.full(len(trialLog),np.nan)
-laserFrameTimes = np.full(len(trialLog),np.nan)
+laserFrames = np.full(len(trialLog),np.nan)
 for i,trial in enumerate(trialLog):
     events = [event[0] for event in trial['events']]
     for event,epoch,t,frame in trial['events']:
@@ -121,13 +92,46 @@ for i,trial in enumerate(trialLog):
                 if laser['actual_change_frame']==changeFrames[i]:
                     laserTrials[i] = True
                     laserOffset[i] = laser['actual_layzer_frame']-laser['actual_change_frame']
-                    laserFrameTimes[i] = vsyncTimes[laser['actual_layzer_frame']]
+                    laserFrames[i] = laser['actual_layzer_frame']
                     break
     if len(trial['rewards']) > 0:
         rewardTimes[i] = trial['rewards'][0][1]
         autoReward[i] = trial['trial_params']['auto_reward']
         
-  
+
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
+offsets = np.concatenate((np.unique(laserOffset[~np.isnan(laserOffset)]),[np.nan]))
+x = (offsets-1)/frameRate*1000
+x[-1] = x[-2]*1.5
+for trials,resp,clr,lbl in zip((changeTrials,catchTrials),(hit,falseAlarm),'kr',('hit','false alarm')):
+    r = []
+    for offset in offsets:
+        i = trials & np.isnan(laserOffset) if np.isnan(offset) else trials & (laserOffset==offset)
+        r.append(resp[i].sum()/i.sum())
+        print(offset,resp[i].sum(),i.sum())
+    ax.plot(x,r,'o-',color=clr,label=lbl)
+for side in ('right','top'):
+    ax.spines[side].set_visible(False)
+ax.tick_params(direction='out',top=False,right=False)
+ax.set_ylim([0,1])
+ax.set_xticks(x)
+ax.set_xticklabels([int(i) for i in x[:-1]]+['no opto'])
+ax.set_xlabel('Laser onset relative to change (ms)')
+ax.set_ylabel('Response rate')
+ax.legend()
+
+
+
+
+syncFile = fileIO.getFile('choose sync file',fileType='*.h5')
+syncDataset = sync.Dataset(syncFile)
+
+frameRising, frameFalling = probeSync.get_sync_line_data(syncDataset, 'vsync_stim')
+vsyncTimes = frameFalling[1:] if frameFalling[0] < frameRising[0] else frameFalling
+frameAppearTimes = vsyncTimes + monitorLag
+
+laserRising,laserFalling = probeSync.get_sync_line_data(syncDataset,channel=11)  
 
 laserOnFromChange,laserOffFromChange = [t-vsyncTimes[changeFrames[laserTrials].astype(int)] for t in (laserRising,laserFalling)]
 
@@ -145,8 +149,9 @@ for t,xlbl in zip((laserRising,laserFalling),('onset','offset')):
     ax.set_ylabel('Count')
 
 
-startToChange = np.array(changeTimes)-np.array(trialStartTimes)
-timeBetweenChanges = np.diff(changeTimes)
+ind = changeTrials | catchTrials
+startToChange = changeTimes[ind]-trialStartTimes[ind]
+timeBetweenChanges = np.diff(changeTimes[ind])
 
 for t,xlbl in zip((startToChange,timeBetweenChanges),('Time from trial start to change','Time between changes')):
     fig = plt.figure()
@@ -157,23 +162,7 @@ for t,xlbl in zip((startToChange,timeBetweenChanges),('Time from trial start to 
     ax.tick_params(direction='out',top=False,right=False)
     ax.set_xlabel(xlbl+' (s)')
     ax.set_ylabel('Count')
-
-offsets = list(set(laserOffset[~np.isnan(laserOffset)]))+[np.nan]
-x = offsets[:-1]+[offsets[-2]*2]
-ax = plt.subplot(1,1,1)
-for trials,resp,clr in zip((changeTrials,catchTrials),(hit,falseAlarm),'kr'):
-    r = []
-    for offset in offsets:
-        i = trials & np.isnan(laserOffset) if np.isnan(offset) else trials & (laserOffset==offset)
-        r.append(resp[i].sum()/i.sum())
-        print(offset,resp[i].sum(),i.sum())
-    ax.plot(x,r,'o-',color=clr)
-ax.set_ylim([0,0.6])
         
-
-
-
-
 
 
 
