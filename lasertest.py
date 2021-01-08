7cd18ce715538d705ee6660c1d0ada03618b1e7b
Sam Gale <samueldgale@gmail.com>
2021-01-08 12:15:10 -0800

    Update lasertest.py

--- lasertest.py
+++ lasertest.py
@@ -24,152 +24,206 @@ def getLickLatency(lickTimes,eventTimes,offset=0):
     return lickLat
 
 
-pklFile = fileIO.getFile('choose pkl file',fileType='*.pkl')
-pkl = pd.read_pickle(pklFile)
-
-params = pkl['items']['behavior']['params']
-if params['periodic_flash'] is not None:
-    flashDur,grayDur = params['periodic_flash']
-    flashInterval = flashDur + grayDur
-
 frameRate = 60
-monitorLag = params['laser_params']['monitor_lag']/frameRate
 
-trialLog = pkl['items']['behavior']['trial_log']
-laserLog = pkl['items']['behavior']['layzer_trials']
-changeLog = pkl['items']['behavior']['stimuli']['images']['change_log']
-             
-trialStartTimes = np.full(len(trialLog),np.nan)
-trialStartFrames = np.full(len(trialLog),np.nan)
-trialEndTimes = np.full(len(trialLog),np.nan)
-abortedTrials = np.zeros(len(trialLog),dtype=bool)
-abortTimes = np.full(len(trialLog),np.nan)
-scheduledChangeTimes = np.full(len(trialLog),np.nan)
-changeTimes = np.full(len(trialLog),np.nan)
-changeFrames = np.full(len(trialLog),np.nan)
-changeTrials = np.zeros(len(trialLog),dtype=bool)
-catchTrials = np.zeros(len(trialLog),dtype=bool)
-preChangeImage = ['' for _ in range(len(trialLog))]
-changeImage = ['' for _ in range(len(trialLog))]
-rewardTimes = np.full(len(trialLog),np.nan)
-autoReward = np.zeros(len(trialLog),dtype=bool)
-hit = np.zeros(len(trialLog),dtype=bool)
-miss = np.zeros(len(trialLog),dtype=bool)
-falseAlarm = np.zeros(len(trialLog),dtype=bool)
-correctReject = np.zeros(len(trialLog),dtype=bool)
-laser = np.full(len(trialLog),np.nan)
-laserOffset = np.full(len(trialLog),np.nan)
-laserFrames = np.full(len(trialLog),np.nan)
-for i,trial in enumerate(trialLog):
-    events = [event[0] for event in trial['events']]
-    for event,epoch,t,frame in trial['events']:
-        if event=='trial_start':
-            trialStartTimes[i] = t
-            trialStartFrames[i] = frame
-        elif event=='trial_end':
-            trialEndTimes[i] = t
-        elif event=='stimulus_window' and epoch=='enter':
-            ct = trial['trial_params']['change_time']
-            if params['periodic_flash'] is not None:
-                ct *= flashInterval
-                ct -= params['pre_change_time']-flashInterval
-            scheduledChangeTimes[i] = t + ct
-        elif 'abort' in events:
-            if event=='abort':
-                abortedTrials[i] = True
-                abortTimes[i] = t
-        elif event in ('stimulus_changed','sham_change'):
-            changeTimes[i] = t
-            changeFrames[i] = frame
-        elif event=='hit':
-            hit[i] = True
-        elif event=='miss':
-            miss[i] = True 
-        elif event=='false_alarm':
-            falseAlarm[i] = True
-        elif event=='rejection':
-            correctReject[i] = True
-    if not abortedTrials[i]:
-        if trial['trial_params']['catch']:
-            catchTrials[i] = True
-        else:
-            if len(trial['stimulus_changes'])>0:
-                changeTrials[i] = True
-                preChangeImage[i] = trial['stimulus_changes'][0][0][0]
-                changeImage[i] = trial['stimulus_changes'][0][1][0]
-        for laserTrial in laserLog:
-            if 'actual_change_frame' in laserTrial and 'actual_layzer_frame' in laserTrial:
-                if laserTrial['actual_change_frame']==changeFrames[i]:
-                    laserOffset[i] = laserTrial['actual_layzer_frame']-laserTrial['actual_change_frame']
-                    laserFrames[i] = laserTrial['actual_layzer_frame']
-                    if 'laser' in laserTrial:
-                        laser[i] = laserTrial['laser']
-                    break
-    if len(trial['rewards']) > 0:
-        rewardTimes[i] = trial['rewards'][0][1]
-        autoReward[i] = trial['trial_params']['auto_reward']
-        
-frameIntervals = pkl['items']['behavior']['intervalsms']/1000
-frameTimes = np.concatenate(([0],np.cumsum(frameIntervals)))
-frameTimes += trialStartTimes[0] - frameTimes[int(trialStartFrames[0])]
-
-lickFrames = pkl['items']['behavior']['lick_sensors'][0]['lick_events']
-lickTimes = frameTimes[lickFrames]
-        
-
-lasers = [np.nan] if all(np.isnan(laser)) else np.unique(laser[~np.isnan(laser)])
-for las in lasers:
-    fig = plt.figure()
-    fig.text(0.5,0.99,'laser '+str(int(las)),va='top',ha='center',fontsize=10)
-    ax = fig.add_subplot(1,1,1)
-    offsets = np.concatenate((np.unique(laserOffset[~np.isnan(laserOffset)]),[np.nan]))
-    if len(offsets)>1:
-        xticks = (offsets-1)/frameRate*1000
-        xticks[-1] = xticks[-2]*1.5
+pklFiles = []
+while True:
+    f = fileIO.getFile('choose pkl file',fileType='*.pkl')
+    if f!='':
+        pklFiles.append(f)
     else:
-        xticks = [0]
-    laserTrials = np.isnan(laser) | (laser==las)
-    for trials,resp,clr,lbl,txty in zip((changeTrials,catchTrials),(hit,falseAlarm),'kr',('hit','false alarm'),(1.05,1.0)):
-        r = []
-        for offset,x in zip(offsets,xticks):
-            offsetTrials = np.isnan(laserOffset) if np.isnan(offset) else laserOffset==offset
-            i = trials & laserTrials & offsetTrials
-            n = i.sum()
-            r.append(resp[i].sum()/n)
-            fig.text(x,txty,str(n),color=clr,transform=ax.transData,va='bottom',ha='center',fontsize=8)
-        ax.plot(xticks,r,'o-',color=clr,label=lbl)
-    for side in ('right','top'):
-        ax.spines[side].set_visible(False)
-    ax.tick_params(direction='out',top=False,right=False)
-    ax.set_ylim([0,1])
-    ax.set_xticks(xticks)
-    ax.set_xticklabels([int(i) for i in xticks[:-1]]+['no opto'])
-    ax.set_xlabel('Laser onset relative to change (ms)')
-    ax.set_ylabel('Response rate')
-    ax.legend()
+        break
 
-    fig = plt.figure()
-    ax = fig.add_subplot(1,1,1)
-    for w in params['response_window']:
-        ax.plot([0,xticks[-1]],[w*1000]*2,'--',color='0.75')
-    for trials,resp,clr,lbl in zip((changeTrials,catchTrials),(hit,falseAlarm),'kr',('hit','false alarm')):
-        r = []
-        for offset,x in zip(offsets,xticks):
-            offsetTrials = np.isnan(laserOffset) if np.isnan(offset) else laserOffset==offset
-            i = trials & resp & laserTrials & offsetTrials
-            r.append(1000*getLickLatency(lickTimes,changeTimes[i],params['response_window'][0]))
-            ax.plot(x+np.zeros(len(r[-1])),r[-1],'o',mec=clr,mfc='none')
-        ax.plot(xticks,[np.nanmean(y) for y in r],'o',mec=clr,mfc=clr,label=lbl)
-    for side in ('right','top'):
-        ax.spines[side].set_visible(False)
-    ax.tick_params(direction='out',top=False,right=False)
-    ax.set_ylim([0,900])
-    ax.set_xticks(xticks)
-    ax.set_xticklabels([int(i) for i in xticks[:-1]]+['no opto'])
-    ax.set_xlabel('Laser onset relative to change (ms)')
-    ax.set_ylabel('Reaction time (ms)')
-    ax.set_title('laser '+str(int(las)))
-    ax.legend()
+params = []   
+trialStartTimes = []
+trialStartFrames = []
+trialEndTimes = []
+abortedTrials = []
+abortTimes = []
+scheduledChangeTimes = []
+changeTimes = []
+changeFrames = []
+changeTrials = []
+catchTrials = []
+preChangeImage = []
+changeImage = []
+rewardTimes = []
+autoReward = []
+hit = []
+miss = []
+falseAlarm = []
+correctReject = []
+laser = []
+laserOffset = []
+laserFrames = []
+lickTimes = []
+        
+for f in pklFiles:       
+    pkl = pd.read_pickle(f)
+
+    params.append(pkl['items']['behavior']['params'])
+    if params[-1]['periodic_flash'] is not None:
+        flashDur,grayDur = params[-1]['periodic_flash']
+        flashInterval = flashDur + grayDur
+    
+    monitorLag = params[-1]['laser_params']['monitor_lag']/frameRate
+    
+    trialLog = pkl['items']['behavior']['trial_log']
+    laserLog = pkl['items']['behavior']['layzer_trials']
+    changeLog = pkl['items']['behavior']['stimuli']['images']['change_log']
+                 
+    trialStartTimes.append(np.full(len(trialLog),np.nan))
+    trialStartFrames.append(np.full(len(trialLog),np.nan))
+    trialEndTimes.append(np.full(len(trialLog),np.nan))
+    abortedTrials.append(np.zeros(len(trialLog),dtype=bool))
+    abortTimes.append(np.full(len(trialLog),np.nan))
+    scheduledChangeTimes.append(np.full(len(trialLog),np.nan))
+    changeTimes.append(np.full(len(trialLog),np.nan))
+    changeFrames.append(np.full(len(trialLog),np.nan))
+    changeTrials.append(np.zeros(len(trialLog),dtype=bool))
+    catchTrials.append(np.zeros(len(trialLog),dtype=bool))
+    preChangeImage.append(['' for _ in range(len(trialLog))])
+    changeImage.append(['' for _ in range(len(trialLog))])
+    rewardTimes.append(np.full(len(trialLog),np.nan))
+    autoReward.append(np.zeros(len(trialLog),dtype=bool))
+    hit.append(np.zeros(len(trialLog),dtype=bool))
+    miss.append(np.zeros(len(trialLog),dtype=bool))
+    falseAlarm.append(np.zeros(len(trialLog),dtype=bool))
+    correctReject.append(np.zeros(len(trialLog),dtype=bool))
+    laser.append(np.full(len(trialLog),np.nan))
+    laserOffset.append(np.full(len(trialLog),np.nan))
+    laserFrames.append(np.full(len(trialLog),np.nan))
+    
+    for i,trial in enumerate(trialLog):
+        events = [event[0] for event in trial['events']]
+        for event,epoch,t,frame in trial['events']:
+            if event=='trial_start':
+                trialStartTimes[-1][i] = t
+                trialStartFrames[-1][i] = frame
+            elif event=='trial_end':
+                trialEndTimes[-1][i] = t
+            elif event=='stimulus_window' and epoch=='enter':
+                ct = trial['trial_params']['change_time']
+                if params[-1]['periodic_flash'] is not None:
+                    ct *= flashInterval
+                    ct -= params[-1]['pre_change_time']-flashInterval
+                scheduledChangeTimes[-1][i] = t + ct
+            elif 'abort' in events:
+                if event=='abort':
+                    abortedTrials[-1][i] = True
+                    abortTimes[-1][i] = t
+            elif event in ('stimulus_changed','sham_change'):
+                changeTimes[-1][i] = t
+                changeFrames[-1][i] = frame
+            elif event=='hit':
+                hit[-1][i] = True
+            elif event=='miss':
+                miss[-1][i] = True 
+            elif event=='false_alarm':
+                falseAlarm[-1][i] = True
+            elif event=='rejection':
+                correctReject[-1][i] = True
+        if not abortedTrials[-1][i]:
+            if trial['trial_params']['catch']:
+                catchTrials[-1][i] = True
+            else:
+                if len(trial['stimulus_changes'])>0:
+                    changeTrials[-1][i] = True
+                    preChangeImage[-1][i] = trial['stimulus_changes'][0][0][0]
+                    changeImage[-1][i] = trial['stimulus_changes'][0][1][0]
+            for laserTrial in laserLog:
+                if 'actual_change_frame' in laserTrial and 'actual_layzer_frame' in laserTrial:
+                    if laserTrial['actual_change_frame']==changeFrames[-1][i]:
+                        laserOffset[-1][i] = laserTrial['actual_layzer_frame']-laserTrial['actual_change_frame']
+                        laserFrames[-1][i] = laserTrial['actual_layzer_frame']
+                        if 'laser' in laserTrial:
+                            laser[-1][i] = laserTrial['laser']
+                        break
+        if len(trial['rewards']) > 0:
+            rewardTimes[-1][i] = trial['rewards'][0][1]
+            autoReward[-1][i] = trial['trial_params']['auto_reward']
+            
+    frameIntervals = pkl['items']['behavior']['intervalsms']/1000
+    frameTimes = np.concatenate(([0],np.cumsum(frameIntervals)))
+    frameTimes += trialStartTimes[-1][0] - frameTimes[int(trialStartFrames[-1][0])]
+    
+    lickFrames = pkl['items']['behavior']['lick_sensors'][0]['lick_events']
+    lickTimes.append(frameTimes[lickFrames])
+        
+
+def plotPerformance(params,laser,laserOffset,changeTrials,catchTrials,hit,falseAlarm,changeTimes,lickTimes):
+    if isinstance(params,list):
+        respWin = params[0]['response_window']
+        laser = np.concatenate(laser)
+        laserOffset = np.concatenate(laserOffset)
+        changeTrials = np.concatenate(changeTrials)
+        catchTrials = np.concatenate(catchTrials)
+        hit = np.concatenate(hit)
+        falseAlarm = np.concatenate(falseAlarm)
+        lickLatency = np.concatenate([getLickLatency(lt,ct,respWin[0]) for ct,lt in zip(changeTimes,lickTimes)])
+    else:
+        respWin = params['response_window']
+        lickLatency = getLickLatency(lickTimes,changeTimes,respWin[0])
+    lasers = [np.nan] if all(np.isnan(laser)) else np.unique(laser[~np.isnan(laser)])
+    for las in lasers:
+        fig = plt.figure()
+        fig.text(0.5,0.99,'laser '+str(int(las)),va='top',ha='center',fontsize=10)
+        ax = fig.add_subplot(1,1,1)
+        offsets = np.concatenate((np.unique(laserOffset[~np.isnan(laserOffset)]),[np.nan]))
+        if len(offsets)>1:
+            xticks = (offsets-1)/frameRate*1000
+            xticks[-1] = xticks[-2]*1.5
+        else:
+            xticks = [0]
+        laserTrials = np.isnan(laser) | (laser==las)
+        for trials,resp,clr,lbl,txty in zip((changeTrials,catchTrials),(hit,falseAlarm),'kr',('hit','false alarm'),(1.05,1.0)):
+            r = []
+            for offset,x in zip(offsets,xticks):
+                offsetTrials = np.isnan(laserOffset) if np.isnan(offset) else laserOffset==offset
+                i = trials & laserTrials & offsetTrials
+                n = i.sum()
+                r.append(resp[i].sum()/n)
+                fig.text(x,txty,str(n),color=clr,transform=ax.transData,va='bottom',ha='center',fontsize=8)
+            ax.plot(xticks,r,'o-',color=clr,label=lbl)
+        for side in ('right','top'):
+            ax.spines[side].set_visible(False)
+        ax.tick_params(direction='out',top=False,right=False)
+        ax.set_ylim([0,1])
+        ax.set_xticks(xticks)
+        ax.set_xticklabels([int(i) for i in xticks[:-1]]+['no opto'])
+        ax.set_xlabel('Laser onset relative to change (ms)')
+        ax.set_ylabel('Response rate')
+        ax.legend()
+
+        fig = plt.figure()
+        ax = fig.add_subplot(1,1,1)
+        for w in respWin:
+            ax.plot([0,xticks[-1]],[w*1000]*2,'--',color='0.75')
+        for trials,resp,clr,lbl in zip((changeTrials,catchTrials),(hit,falseAlarm),'kr',('hit','false alarm')):
+            r = []
+            for offset,x in zip(offsets,xticks):
+                offsetTrials = np.isnan(laserOffset) if np.isnan(offset) else laserOffset==offset
+                i = trials & resp & laserTrials & offsetTrials
+                r.append(1000*lickLatency[i])
+                ax.plot(x+np.zeros(len(r[-1])),r[-1],'o',mec=clr,mfc='none')
+            ax.plot(xticks,[np.nanmean(y) for y in r],'o',mec=clr,mfc=clr,label=lbl)
+        for side in ('right','top'):
+            ax.spines[side].set_visible(False)
+        ax.tick_params(direction='out',top=False,right=False)
+        ax.set_ylim([0,900])
+        ax.set_xticks(xticks)
+        ax.set_xticklabels([int(i) for i in xticks[:-1]]+['no opto'])
+        ax.set_xlabel('Laser onset relative to change (ms)')
+        ax.set_ylabel('Reaction time (ms)')
+        ax.set_title('laser '+str(int(las)))
+        ax.legend()
+
+
+for i,_ in enumerate(pklFiles):
+    plotPerformance(params[i],laser[i],laserOffset[i],changeTrials[i],catchTrials[i],hit[i],falseAlarm[i],changeTimes[i],lickTimes[i])
+
+plotPerformance(params,laser,laserOffset,changeTrials,catchTrials,hit,falseAlarm,changeTimes,lickTimes)
+
 
 
 
