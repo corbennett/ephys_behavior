7de77573415d1c558ce864826863924a7230a74f
Sam Gale <samueldgale@gmail.com>
2022-03-09 12:01:51 -0800

    Update contrast_task_analysis.py

--- contrast_task_analysis.py
+++ contrast_task_analysis.py
@@ -93,6 +93,8 @@ class DocData():
                 self.postContrast[i] = trial['stimulus_changes'][0][1][1]['contrast']
                 self.preLabel[i] = self.preImage[i]+' ('+str(self.preContrast[i])+')'
                 self.postLabel[i] = self.postImage[i]+' ('+str(self.postContrast[i])+')'
+                
+        self.engaged = np.array([np.sum(self.hit[self.changeTrials][(self.changeTimes[self.changeTrials]>t-60) & (self.changeTimes[self.changeTrials]<t+60)]) > 1 for t in self.changeTimes])
             
         self.labels = sorted(list(set(self.preLabel+self.postLabel))[1:])
         self.trialCount = np.zeros((len(self.labels),)*2)
@@ -103,8 +105,8 @@ class DocData():
                 img = [lbl[:lbl.find(' (')] for lbl in (preLbl,postLbl)]
                 if img[0] != img[1]:
                     self.imageChange[i,j] = True
-                for pre,post,h,fa in zip(self.preLabel,self.postLabel,self.hit,self.falseAlarm):
-                    if pre==preLbl and post==postLbl:
+                for pre,post,h,fa,eng in zip(self.preLabel,self.postLabel,self.hit,self.falseAlarm,self.engaged):
+                    if pre==preLbl and post==postLbl and eng:
                         self.trialCount[i,j] += 1
                         self.respCount[i,j] += h or fa
         self.respRate = self.respCount/self.trialCount
@@ -147,81 +149,21 @@ if len(behavFiles)>0:
         obj.loadBehavData(f)
         exps.append(obj)
         
-        
-#
-hitRateLow = []
-hitRateHigh = []
-falseAlarmRate = []
 
-hitRateLow.append([])
-hitRateHigh.append([])
-falseAlarmRate.append([])
-for obj in exps[::-1]:
-    r = obj.respCount.copy()
-    r[~obj.imageChange] = 0
-    c = obj.trialCount.copy()
-    c[~obj.imageChange] = 0
-    hitRateLow[-1].append(r[0::2].sum()/c[0::2].sum())
-    hitRateHigh[-1].append(r[1::2].sum()/c[1::2].sum())
-    fa = np.eye(obj.respCount.shape[0]).astype(bool)
-    r = obj.respCount[fa]
-    c = obj.trialCount[fa]
-    falseAlarmRate[-1].append(r.sum()/c.sum())
-    
-dp = []
-for hr in (hitRateHigh,hitRateLow):
-    z = [scipy.stats.norm.ppf(r) for r in (hr,falseAlarmRate)]
-    dp.append(np.mean(z[0]-z[1],axis=0))
-
-    
-xlbl = ('20%','40%','60%','60%','60%')
-    
-fig = plt.figure(figsize=(6,9))
-ax = fig.add_subplot(3,1,1)
-ax.plot(np.mean(hitRateHigh,axis=0),'g',lw=2,label='high contrast')
-ax.plot(np.mean(hitRateLow,axis=0),'m',lw=2,label='low contrast')
-ax.plot(np.mean(falseAlarmRate,axis=0),'0.5',lw=2,label='no change')
-for side in ('right','top'):
-    ax.spines[side].set_visible(False)
-ax.tick_params(direction='out',top=False,right=False)
-ax.set_xticks(np.arange(len(xlbl)))
-ax.set_xticklabels(xlbl)
-ax.set_ylim([0,1])
-ax.set_xlabel('Daily Low Contrast')
-ax.set_ylabel('Response Rate')
-ax.legend()
-plt.tight_layout()
-   
-ax = fig.add_subplot(3,1,2)
-ax.plot(dp[0],'g',lw=2,label='high contrast')
-ax.plot(dp[1],'m',lw=2,label='low contrast')
-for side in ('right','top'):
-    ax.spines[side].set_visible(False)
-ax.tick_params(direction='out',top=False,right=False)
-ax.set_xticks(np.arange(len(xlbl)))
-ax.set_xticklabels(xlbl)
-ax.set_ylim([0,3])
-ax.set_xlabel('Daily Low Contrast')
-ax.set_ylabel('d prime')
-ax.legend()
-plt.tight_layout()
-
-ax = fig.add_subplot(3,1,3)
-ax.plot(100*(1-dp[1]/dp[0]),'k',lw=2)
-for side in ('right','top'):
-    ax.spines[side].set_visible(False)
-ax.tick_params(direction='out',top=False,right=False)
-ax.set_xticks(np.arange(len(xlbl)))
-ax.set_xticklabels(xlbl)
-ax.set_ylim([45,75])
-ax.set_xlabel('Daily Low Contrast')
-ax.set_ylabel('% reduction in d prime')
-plt.tight_layout()
-        
-        
 #
 trialCount,respCount,imageChange = [np.array([getattr(obj,attr) for obj in exps]) for attr in ('trialCount','respCount','imageChange')]
 respRate = respCount/trialCount
+
+labels = [obj.labels for obj in exps]
+
+index = []
+for lbls in labels:
+    index.append([])
+    index[-1].append(lbls.index('im115_r (0.6)'))
+    index[-1].append(lbls.index('im115_r (1.0)'))
+    index[-1].append(lbls.index('im047_r (1.0)'))
+    index[-1] += [i for i in range(4) if i not in index[-1]]
+
 
 r = respCount.copy()
 r[~imageChange] = 0
@@ -230,39 +172,99 @@ c[~imageChange] = 0
 preImgRespRate = r.sum(axis=1)/c.sum(axis=1)
 changeImgRespRate = r.sum(axis=2)/c.sum(axis=2)
 
-labels = exps[0].labels
+imgs = np.unique(exps[0].preImage)[1:]
 contrast = exps[0].preContrast
 contrast = np.unique(contrast[~np.isnan(contrast)])
 
+for r,c,lbls,ind in zip(respCount,trialCount,labels,index):
+    r = r[ind,:]
+    r = r[:,ind]
+    c = c[ind,:]
+    c = c[:,ind]
+    lbls = np.array(lbls)[ind]
+    fig = plt.figure()
+    ax = fig.add_subplot(1,1,1)
+    im = ax.imshow(r/c,clim=[0,1],cmap='magma')
+    for i in range(len(lbls)):
+        for j in range(len(lbls)):
+            ax.text(j,i,str(int(r[i,j]))+'/'+str(int(c[i,j])),color='w',ha='center',va='center')
+    ax.set_xticks(np.arange(len(lbls)))
+    ax.set_xticklabels(lbls,rotation=90)
+    ax.set_xlabel('Pre Image (contrast)')
+    ax.set_xlim([-0.5,len(lbls)-0.5])
+    ax.set_yticks(np.arange(len(lbls)))
+    ax.set_yticklabels(lbls)
+    ax.set_ylabel('Change Image (contrast)')
+    ax.set_ylim([len(lbls)-0.5,-0.5])
+    ax.set_title('Response Rate')
+    cb = plt.colorbar(im,ax=ax,fraction=0.02,pad=0.15)
+    plt.tight_layout()
+
+
+r = np.nanmean(respRate,axis=0)
+r = r[ind,:]
+r = r[:,ind]
+lbls = np.array(labels[0])[ind]
+lbls[-1] = 'novel (1.0)'
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
-r = np.nanmean(respRate,axis=0)
-im = ax.imshow(r,clim=[0,r.max()],cmap='gray')
-ax.set_xticks(np.arange(len(labels)))
-ax.set_xticklabels(labels,rotation=90)
+im = ax.imshow(r,clim=[0,1],cmap='magma')
+for i in range(len(lbls)):
+    for j in range(len(lbls)):
+        ax.text(j,i,str(round(r[i,j],2)),color='w',ha='center',va='center')
+ax.set_xticks(np.arange(len(lbls)))
+ax.set_xticklabels(lbls,rotation=90)
 ax.set_xlabel('Pre Image (contrast)')
-ax.set_xlim([-0.5,len(labels)-0.5])
-ax.set_yticks(np.arange(len(labels)))
-ax.set_yticklabels(labels)
+ax.set_xlim([-0.5,len(lbls)-0.5])
+ax.set_yticks(np.arange(len(lbls)))
+ax.set_yticklabels(lbls)
 ax.set_ylabel('Change Image (contrast)')
-ax.set_ylim([len(labels)-0.5,-0.5])
+ax.set_ylim([len(lbls)-0.5,-0.5])
 ax.set_title('Response Rate')
 cb = plt.colorbar(im,ax=ax,fraction=0.02,pad=0.15)
 plt.tight_layout()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 fig = plt.figure()
 ax = fig.add_subplot(1,1,1)
 ax.plot([0,1],[0,1],'--',color='0.5')
 for rr,mfc,lbl in zip((preImgRespRate,changeImgRespRate),('none','k'),('pre image','change image')):
-    ax.plot(rr[:,1::2].mean(axis=0),rr[:,::2].mean(axis=0),'o',mec='k',mfc=mfc,label=lbl)
+    x = rr[:,1::2].mean(axis=0)
+    y = rr[:,::2].mean(axis=0)
+    ax.plot(x,y,'o',mec='k',mfc=mfc,label=lbl)
+    for i,j,img in zip(y,x,imgs):
+#        if lbl=='change image' and img=='im111_r':
+#            ax.text(j,i-0.02,img[:-2],ha='center',va='top')
+#        else:
+        ax.text(j,i+0.02,img[:-2],ha='center',va='bottom')
 for side in ('right','top'):
     ax.spines[side].set_visible(False)
 ax.tick_params(direction='out',top=False,right=False)
 ax.set_xlim([0,1.02])
 ax.set_ylim([0,1.02])
 ax.set_aspect('equal')
-ax.set_xlabel(str(int(contrast[1]*100))+'% contrast')
-ax.set_ylabel(str(int(contrast[0]*100))+'% contrast')
+ax.set_xlabel('Response rate, '+str(int(contrast[1]*100))+'% contrast')
+ax.set_ylabel('Response rate, '+str(int(contrast[0]*100))+'% contrast')
 leg = ax.legend(loc='upper left')
 plt.tight_layout()
 
@@ -276,8 +278,97 @@ ax.tick_params(direction='out',top=False,right=False)
 ax.set_xlim([0,1.02])
 ax.set_ylim([0,1.02])
 ax.set_aspect('equal')
-ax.set_xlabel(str(int(contrast[1]*100))+'% contrast')
-ax.set_ylabel(str(int(contrast[0]*100))+'% contrast')
+ax.set_xlabel('Response rate, '+str(int(contrast[1]*100))+'% contrast')
+ax.set_ylabel('Response rate, '+str(int(contrast[0]*100))+'% contrast')
+plt.tight_layout()
+
+
+
+#
+hitRateLow = []
+hitRateHigh = []
+falseAlarmRate = []
+lowTrials = []
+highTrials = []
+catchTrials = []
+
+hitRateLow.append([])
+hitRateHigh.append([])
+falseAlarmRate.append([])
+lowTrials.append([])
+highTrials.append([])
+catchTrials.append([])
+for obj in exps[::-1]:
+    r = obj.respCount.copy()
+    r[~obj.imageChange] = 0
+    c = obj.trialCount.copy()
+    c[~obj.imageChange] = 0
+    hitRateLow[-1].append(r[0::2].sum()/c[0::2].sum())
+    hitRateHigh[-1].append(r[1::2].sum()/c[1::2].sum())
+    lowTrials[-1].append(c[0::2].sum())
+    highTrials[-1].append(c[1::2].sum())
+    fa = np.eye(obj.respCount.shape[0]).astype(bool)
+    r = obj.respCount[fa]
+    c = obj.trialCount[fa]
+    falseAlarmRate[-1].append(r.sum()/c.sum())
+    catchTrials[-1].append(c.sum())
+    
+dp = []
+for hr,n in zip((hitRateHigh,hitRateLow),(highTrials,lowTrials)):
+    hr = np.array(hr)
+    hr[hr==0] = 0.5/np.array(n)[hr==0]
+    hr[hr==1] = 1-0.5/np.array(n)[hr==1]
+    fa = np.array(falseAlarmRate)
+    fa[fa==0] = 0.5/np.array(catchTrials)[fa==0]
+    fa[fa==1] = 1-0.5/np.array(catchTrials)[fa==1]
+    z = [scipy.stats.norm.ppf(r) for r in (hr,fa)]
+    dp.append(np.mean(z[0]-z[1],axis=0))
+
+    
+xlbl = ('20%','40%')+('60%',)*(len(exps)-2)
+
+xlbl = [1,2,3]
+    
+fig = plt.figure(figsize=(6,9))
+ax = fig.add_subplot(3,1,1)
+ax.plot(np.mean(hitRateHigh,axis=0),'g',lw=2,label='high contrast')
+ax.plot(np.mean(hitRateLow,axis=0),'m',lw=2,label='low contrast')
+ax.plot(np.mean(falseAlarmRate,axis=0),'0.5',lw=2,label='no change')
+for side in ('right','top'):
+    ax.spines[side].set_visible(False)
+ax.tick_params(direction='out',top=False,right=False)
+ax.set_xticks(np.arange(len(xlbl)))
+ax.set_xticklabels(xlbl)
+ax.set_ylim([0,1])
+ax.set_xlabel('Session')
+ax.set_ylabel('Response Rate')
+ax.legend()
+plt.tight_layout()
+   
+ax = fig.add_subplot(3,1,2)
+ax.plot(dp[0],'g',lw=2,label='high contrast')
+ax.plot(dp[1],'m',lw=2,label='low contrast')
+for side in ('right','top'):
+    ax.spines[side].set_visible(False)
+ax.tick_params(direction='out',top=False,right=False)
+ax.set_xticks(np.arange(len(xlbl)))
+ax.set_xticklabels(xlbl)
+ax.set_ylim([0,3])
+ax.set_xlabel('Session')
+ax.set_ylabel('d prime')
+ax.legend()
+plt.tight_layout()
+
+ax = fig.add_subplot(3,1,3)
+ax.plot(100*(1-dp[1]/dp[0]),'k',lw=2)
+for side in ('right','top'):
+    ax.spines[side].set_visible(False)
+ax.tick_params(direction='out',top=False,right=False)
+ax.set_xticks(np.arange(len(xlbl)))
+ax.set_xticklabels(xlbl)
+ax.set_ylim([35,65])
+ax.set_xlabel('Session')
+ax.set_ylabel('% reduction in d prime')
 plt.tight_layout()
 
 
